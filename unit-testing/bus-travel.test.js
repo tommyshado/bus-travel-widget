806b7b3d22df3d0fa9de7c49cff39680919b9df2
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-31 13:45:03 +0200

    added more test cases for normal bus time and not normal bus time

--- unit-testing/bus-travel.test.js
+++ unit-testing/bus-travel.test.js
@@ -1,13 +1,40 @@
 describe('The Bus Travel App functionality unit testing', function() {
     it('should return 0 when there are no number of points for a single trip', function() {
         const busTravel = BusTravelApp();
-        busTravel.amountOfSingleTrips(0);
-        assert.equal(0, busTravel.getSingleTripAmount());
+        busTravel.pointsOfSingleTrips(0);
+        assert.equal(0, busTravel.getSingleTripPoints());
     });
 
-    it('should return 10 when there are 10 number of points for a single trip', function() {
+    it('should return 1 when there is 1 number of point for a single trip', function() {
         const busTravel = BusTravelApp();
-        busTravel.amountOfSingleTrips(10);
-        assert.equal(10, busTravel.getSingleTripAmount());
+        busTravel.pointsOfSingleTrips(1, 'khayeltsha');
+        assert.equal(1, busTravel.getSingleTripPoints());
+    });
+
+    it('should return 1 when there is 1 number of point for a single trip', function() {
+        const busTravel = BusTravelApp();
+        busTravel.pointsOfSingleTrips(1, 'dunoon');
+        assert.equal(1, busTravel.getSingleTripPoints());
+    });
+
+    it('should be able to calculate the single trip cost for a start location when it"s the normal time', function() {
+        const busTravel = BusTravelApp();
+        busTravel.pointsOfSingleTrips(1, 'khayelitsha');
+        busTravel.singleTripCost();
+        assert.equal(40, busTravel.getSingleTripCost());
+    });
+
+    it('should be able to calculate the single trip cost for a start location when it"s not normal time', function() {
+        const busTravel = BusTravelApp();
+        busTravel.pointsOfSingleTrips(1, 'khayelitsha');
+        busTravel.singleTripCost(true);
+        assert.equal(90, busTravel.getSingleTripCost());
+    });
+
+    it('should be able to calculate the single trip cost for a start location when it"s not normal time', function() {
+        const busTravel = BusTravelApp();
+        busTravel.pointsOfSingleTrips(1, 'mitchells plain');
+        busTravel.singleTripCost(true);
+        assert.equal(67.5, busTravel.getSingleTripCost());
     });
 })
