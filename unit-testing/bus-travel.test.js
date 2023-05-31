928f9a3cd8003b0b3638bdb2fcd032b2478fe7d1
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-31 12:45:01 +0200

    changed my functions names in my test cases and added one more test case

--- unit-testing/bus-travel.test.js
+++ unit-testing/bus-travel.test.js
@@ -1,7 +1,13 @@
 describe('The Bus Travel App functionality unit testing', function() {
-    it('should return 0 when there are no number of points', function() {
+    it('should return 0 when there are no number of points for a single trip', function() {
         const busTravel = BusTravelApp();
-        busTravel.setAmountOfPoints(0);
-        assert.equal(0, busTravel.getAmountOfPoints());
+        busTravel.amountOfSingleTrips(0);
+        assert.equal(0, busTravel.getSingleTripAmount());
+    });
+
+    it('should return 10 when there are 10 number of points for a single trip', function() {
+        const busTravel = BusTravelApp();
+        busTravel.amountOfSingleTrips(10);
+        assert.equal(10, busTravel.getSingleTripAmount());
     });
 })
