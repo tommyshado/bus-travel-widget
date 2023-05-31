bbe65e0757c08622e4ba0f1486570f6ba13927f9
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-31 12:44:25 +0200

    created an if else statement in my calculate button to display the number of single trips and used local storage to make it visible after a refresh of the screen

--- dom/bus-travel.dom.js
+++ dom/bus-travel.dom.js
@@ -3,17 +3,39 @@ const numberOfPoints = document.querySelector('.numberOfPoints');
 const destination = document.querySelector('#destination');
 
 // get the calculate dom elements reference for the bus travel widget
+const singleTripsNum = document.querySelector('.singleTripsNumber');
+
+// buttons
 const calculateBtn = document.querySelector('.calculateBtn');
 
 
 // variable to be reassigned with values that we will get from the local storage and then pass them into the factory function as arguments
+let singleTrip = 0;
+
+
+// local storage code
+if (localStorage['singleTrips']) {
+    // get the value in the local storage
+    singleTrip = Number(localStorage['singleTrips']);
+    singleTripsNum.innerHTML = singleTrip;
+}
 
 // factory function instance for the bus travel widget
+const busTravelAppInstance = BusTravelApp(singleTrip);
 
 // events elements
 calculateBtn.addEventListener('click', function() {
     // get the reference to the peak and off peak radio buttons
     const peakOffPeakRadio = document.querySelector('input[name="peakOffPeak"]:checked');
     // get reference to the checkbox of the return trip
-    const returnTripCheckbox = document.querySelector('input[name="returnTripCheckbox"]');
+    const returnTripCheckbox = document.querySelector('input[name="returnTripCheckbox"]:checked');
+
+    if (returnTripCheckbox) {
+        // 
+    } else {
+        // get the value from the input text box
+        busTravelAppInstance.amountOfSingleTrips(numberOfPoints.value);
+        singleTripsNum.innerHTML = busTravelAppInstance.getSingleTripAmount();
+        localStorage.setItem('singleTrips', singleTripsNum.innerHTML);
+    }
 });
