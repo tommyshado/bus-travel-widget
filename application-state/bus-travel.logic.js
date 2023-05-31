6181de587cf38079339c0eb0b35943c79b7f77ee
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-31 10:43:53 +0200

    pseudo code

---
+++ application-state/bus-travel.logic.js
@@ -0,0 +1,15 @@
+// for the Bus Travel Widget : 
+
+// I am required to create the bus travel widget, that will calculate the cost price for a single trip from a desired destionation. 
+// Destinations have different cost prices:
+    //  Khayelitsha :       R40
+    // Dunoon :             R25
+    // Mitchells Plain :    R30
+
+// there is also a peak hour increase which is charged at 25% per destination.
+
+// I am also required to calculate the total number of trips a client an take based on the number of points provided and everytime she travels the total number of trips get decremented.
+// also judging from the total number of trips available we should be able to determine whether she could take a return trip or not
+// the widget should should number of return trips she can make 
+
+// I am also required to calculate and show the cost per return trip.
---
+++ dom/bus-travel.dom.js
@@ -0,0 +1,11 @@
+// get input reference elements for the bus travel widget
+
+// get the calculate dom elements reference for the bus travel widget
+
+
+// variable to be reassigned with values that we will get from the local storage and then pass them into the factory function as arguments
+
+// factory function instance for the bus travel widget
+
+// events elements
+
