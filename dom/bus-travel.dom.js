47c25306aefc1fe910a3f8e53197d71d56de0b15
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-31 14:08:41 +0200

    used local storage to make the cost of a single trip visible

--- dom/bus-travel.dom.js
+++ dom/bus-travel.dom.js
@@ -4,6 +4,7 @@ const destination = document.querySelector('#destination');
 
 // get the calculate dom elements reference for the bus travel widget
 const singleTripsNum = document.querySelector('.singleTripsNumber');
+const singleTripPrice = document.querySelector('.singleTripsPrice');
 
 // buttons
 const calculateBtn = document.querySelector('.calculateBtn');
@@ -11,6 +12,7 @@ const calculateBtn = document.querySelector('.calculateBtn');
 
 // variable to be reassigned with values that we will get from the local storage and then pass them into the factory function as arguments
 let singleTrip = 0;
+let singleTripCostVar = 0;
 
 
 // local storage code
@@ -20,8 +22,13 @@ if (localStorage['singleTrips']) {
     singleTripsNum.innerHTML = singleTrip;
 }
 
+if (localStorage['singleTripCost']) {
+    singleTripCostVar = Number(localStorage['singleTripCost']);
+    singleTripPrice.innerHTML = singleTripCostVar;
+}
+
 // factory function instance for the bus travel widget
-const busTravelAppInstance = BusTravelApp(singleTrip);
+const busTravelAppInstance = BusTravelApp(singleTrip, singleTripCostVar);
 
 // events elements
 calculateBtn.addEventListener('click', function() {
@@ -30,12 +37,19 @@ calculateBtn.addEventListener('click', function() {
     // get reference to the checkbox of the return trip
     const returnTripCheckbox = document.querySelector('input[name="returnTripCheckbox"]:checked');
 
-    if (returnTripCheckbox) {
+    if (returnTripCheckbox && peakOffPeakRadio) {
         // 
-    } else {
+    } else if (peakOffPeakRadio) {
         // get the value from the input text box
         busTravelAppInstance.pointsOfSingleTrips(numberOfPoints.value, destination.value);
-        singleTripsNum.innerHTML = busTravelAppInstance.getSingleTripAmount();
+        singleTripsNum.innerHTML = busTravelAppInstance.getSingleTripPoints();
+
+        // single trip cost
+        busTravelAppInstance.singleTripCost(peakOffPeakRadio.value);
+        singleTripPrice.innerHTML = busTravelAppInstance.getSingleTripCost();
+
+        // setting values in the local storage
+        localStorage.setItem('singleTripCost', singleTripPrice.innerHTML);
         localStorage.setItem('singleTrips', singleTripsNum.innerHTML);
     }
 });
