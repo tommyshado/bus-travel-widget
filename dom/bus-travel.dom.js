ed88d64b38d64dcf97b9a2a1bdb93e3a57a0e99d
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-31 11:31:22 +0200

    got reference to the radio buttons and checkbox

--- dom/bus-travel.dom.js
+++ dom/bus-travel.dom.js
@@ -1,7 +1,9 @@
 // get input reference elements for the bus travel widget
 const numberOfPoints = document.querySelector('.numberOfPoints');
+const destination = document.querySelector('#destination');
 
 // get the calculate dom elements reference for the bus travel widget
+const calculateBtn = document.querySelector('.calculateBtn');
 
 
 // variable to be reassigned with values that we will get from the local storage and then pass them into the factory function as arguments
@@ -9,4 +11,9 @@ const numberOfPoints = document.querySelector('.numberOfPoints');
 // factory function instance for the bus travel widget
 
 // events elements
-
+calculateBtn.addEventListener('click', function() {
+    // get the reference to the peak and off peak radio buttons
+    const peakOffPeakRadio = document.querySelector('input[name="peakOffPeak"]:checked');
+    // get reference to the checkbox of the return trip
+    const returnTripCheckbox = document.querySelector('input[name="returnTripCheckbox"]');
+});
